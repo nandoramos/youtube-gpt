6e01460994632a34715e4f083ccba3e75a002408
Fernando Ramos <nando@tarmac.io>
2023-09-08 14:44:06 -0300

    remove dist folder

--- youtube-gpt/dist/auth/users.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from "typeorm";
-import { User } from "./user.entity";
-import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
-export declare class UsersRepository extends Repository<User> {
-    createUser(authCredentialsDto: AuthCredentialsDto): Promise<void>;
-}
